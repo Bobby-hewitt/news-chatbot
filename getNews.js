6fbe2f078cf45f39d5935c566cfc77e0323ac270
Robert Hewitt <roberthewitt@Bobbys-MBP.home>
2017-08-29 20:22:34 +0100

    https

--- getNews.js
+++ getNews.js
@@ -3,22 +3,22 @@ const Feed = require('rss-to-json')
 
 module.exports = function(){
 	let feeds = {
-		'Top Stories': 'http://feeds.bbci.co.uk/news/rss.xml?edition=uk',
-		'World': 'http://feeds.bbci.co.uk/news/world/rss.xml?edition=uk',
-		'UK': 'http://feeds.bbci.co.uk/news/uk/rss.xml?edition=uk',
-		'England': 'http://feeds.bbci.co.uk/news/england/rss.xml?edition=uk',
-		'NorthernIreland': 'http://feeds.bbci.co.uk/news/northern_ireland/rss.xml?edition=uk',
-		'Scotland': 'http://feeds.bbci.co.uk/news/scotland/rss.xml?edition=uk',
-		'Wales': 'http://feeds.bbci.co.uk/news/wales/rss.xml?edition=uk',
-		'Business': 'http://feeds.bbci.co.uk/news/business/rss.xml?edition=uk',
-		'Politics': 'http://feeds.bbci.co.uk/news/politics/rss.xml?edition=uk',
-		'Health': 'http://feeds.bbci.co.uk/news/health/rss.xml?edition=uk',
-		'Education': 'http://feeds.bbci.co.uk/news/education/rss.xml?edition=uk',
-		'Science': 'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml?edition=uk',
-		'Technology': 'http://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk',
-		'Entertainment': 'http://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml?edition=uk',
-		'HaveYourSay': 'http://feeds.bbci.co.uk/news/have_your_say/rss.xml?edition=uk',
-		'Gossip': 'http://feeds.bbci.co.uk/news/magazine/rss.xml',
+		'Top Stories': 'https://feeds.bbci.co.uk/news/rss.xml?edition=uk',
+		'World': 'https://feeds.bbci.co.uk/news/world/rss.xml?edition=uk',
+		'UK': 'https://feeds.bbci.co.uk/news/uk/rss.xml?edition=uk',
+		'England': 'https://feeds.bbci.co.uk/news/england/rss.xml?edition=uk',
+		'NorthernIreland': 'https://feeds.bbci.co.uk/news/northern_ireland/rss.xml?edition=uk',
+		'Scotland': 'https://feeds.bbci.co.uk/news/scotland/rss.xml?edition=uk',
+		'Wales': 'https://feeds.bbci.co.uk/news/wales/rss.xml?edition=uk',
+		'Business': 'https://feeds.bbci.co.uk/news/business/rss.xml?edition=uk',
+		'Politics': 'https://feeds.bbci.co.uk/news/politics/rss.xml?edition=uk',
+		'Health': 'https://feeds.bbci.co.uk/news/health/rss.xml?edition=uk',
+		'Education': 'https://feeds.bbci.co.uk/news/education/rss.xml?edition=uk',
+		'Science': 'https://feeds.bbci.co.uk/news/science_and_environment/rss.xml?edition=uk',
+		'Technology': 'https://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk',
+		'Entertainment': 'https://feeds.bbci.co.uk/news/entertainment_and_arts/rss.xml?edition=uk',
+		'HaveYourSay': 'https://feeds.bbci.co.uk/news/have_your_say/rss.xml?edition=uk',
+		'Gossip': 'https://feeds.bbci.co.uk/news/magazine/rss.xml',
 	}
 
 
